f5a8c170327acdb712c1ab8b22a8ba7c380e81e1
jieun-Seo <qhskdishfwkd@naver.com>
2023-11-28 17:03:22 +0900

    초기 프로젝트

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        강혜경 너 때문이야~ 무니야~
+      </header>
+    </div>
+  );
+}
+
+export default App;
